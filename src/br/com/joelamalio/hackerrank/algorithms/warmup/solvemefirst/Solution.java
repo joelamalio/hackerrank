9ec37fbe5f1fb985c5fc438a13bd4a286c406c80
Joel Amálio <joel.amalio@gmail.com>
2018-03-12 17:33:00 -0300

    Solução - Solve Me First.

--- src/br/com/joelamalio/hackerrank/algorithms/warmup/solvemefirst/Solution.java
+++ src/br/com/joelamalio/hackerrank/algorithms/warmup/solvemefirst/Solution.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Solution {
 
 	static int solveMeFirst(int a, int b) {
-		// Hint: Type return a+b; below
-
+		return a+b;
 	}
 
 	public static void main(String[] args) {
