666f8df4bc685022701879d4e895d8739c2cdc38
Joel Amálio <joel.amalio@gmail.com>
2018-03-13 23:08:31 -0300

    Solução - Grading Students.

---
+++ src/br/com/joelamalio/hackerrank/algorithms/implementation/gradingstudents/Solution.java
@@ -0,0 +1,43 @@
+package br.com.joelamalio.hackerrank.algorithms.implementation.gradingstudents;
+
+import java.util.Scanner;
+
+public class Solution {
+	
+	static int[] solve(int[] grades) {
+		int n = grades.length;
+		int points;
+		
+		for (int i = 0; i < n; i++) {
+			points = grades[i];
+			
+			if (points >= 38) {
+				if (points % 5 == 3) {
+					points += 2; 
+				} else if (points % 5 == 4) {
+					points += 1; 
+				}
+				
+				grades[i] = points;
+			}
+			
+		}
+		
+		return grades;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] grades = new int[n];
+		for (int grades_i = 0; grades_i < n; grades_i++) {
+			grades[grades_i] = in.nextInt();
+		}
+		int[] result = solve(grades);
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i] + (i != result.length - 1 ? "\n" : ""));
+		}
+		System.out.println("");
+
+	}
+}
