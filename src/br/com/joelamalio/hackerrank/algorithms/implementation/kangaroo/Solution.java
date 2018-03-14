b3d7a3540b5570be358a1ecae2d7ec74974c609d
Joel Amálio <joel.amalio@gmail.com>
2018-03-14 20:07:07 -0300

    Solução - Kangaroo.

---
+++ src/br/com/joelamalio/hackerrank/algorithms/implementation/kangaroo/Solution.java
@@ -0,0 +1,39 @@
+package br.com.joelamalio.hackerrank.algorithms.implementation.kangaroo;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	static String kangaroo(int x1, int v1, int x2, int v2) {
+		long currentPosition1;
+		long currentPosition2;
+
+		for (int i = 0; i < 10000; i++) {
+			currentPosition1 = x1 + v1 * i;
+			currentPosition2 = x2 + v2 * i;
+						
+			if (currentPosition1 == currentPosition2) {
+				return "YES";
+			}
+			
+		}
+		
+		return "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        String result = kangaroo(x1, v1, x2, v2);
+        System.out.println(result);
+    }
+    
+    /*
+	 * 0 3 4 2 = YES
+	 * 0 2 5 3 = NO
+	 * 21 6 47 3 = NO
+	 */
+}
