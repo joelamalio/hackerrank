2f513e28eb50c8daa7a328f1c9fc7b397685fe3e
Joel Amálio <joel.amalio@gmail.com>
2018-03-13 22:07:59 -0300

    Solução - Birthday Cake Candles.

---
+++ src/br/com/joelamalio/hackerrank/algorithms/warmup/birthdaycakecandles/Solution.java
@@ -0,0 +1,48 @@
+package br.com.joelamalio.hackerrank.algorithms.warmup.birthdaycakecandles;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import br.com.joelamalio.hackerrank.algorithms.warmup.util.Util;
+
+public class Solution {
+	
+	static int birthdayCakeCandles(int n, int[] ar) {
+		Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+		
+		for (int i = 0; i < n; i++) {
+			int key = ar[i];
+			if (counts.containsKey(key)) {
+				counts.put(key, counts.get(key) + 1);
+			} else {
+				counts.put(key, 1);
+			}
+		}
+		
+		return Collections.max(counts.values());
+	}
+	
+	public static void main(String[] args) throws Exception {
+		/*
+		 * 0 => 2
+		 */
+		int[] a = Util.getArray(Solution.class, "test-case-0");
+		int result = birthdayCakeCandles(a.length, a);
+		System.out.println(result);
+	}
+
+
+	/*
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] ar = new int[n];
+		for (int ar_i = 0; ar_i < n; ar_i++) {
+			ar[ar_i] = in.nextInt();
+		}
+		int result = birthdayCakeCandles(n, ar);
+		System.out.println(result);
+	}
+	*/
+}
