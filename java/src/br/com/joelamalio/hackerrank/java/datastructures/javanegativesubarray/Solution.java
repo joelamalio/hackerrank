c44d6bad4ddc9f609569c3e8d4cbbcb830bc2a09
Joel Amálio <joel.amalio@gmail.com>
2018-04-06 19:44:04 -0300

    Solution: Java Subarray.

---
+++ java/src/br/com/joelamalio/hackerrank/java/datastructures/javanegativesubarray/Solution.java
@@ -0,0 +1,30 @@
+package br.com.joelamalio.hackerrank.java.datastructures.javanegativesubarray;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int array[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			array[i] = scan.nextInt();
+		}
+		
+		int amount = 0;
+		
+		for (int i = 0; i < n; i++) {
+			int sum = 0;
+			for (int j = i; j < n; j++) {
+				sum += array[j]; 
+				if (sum < 0) {
+					amount++;
+				}
+			}
+		}
+		
+		System.out.println(amount);
+    }
+	
+}
