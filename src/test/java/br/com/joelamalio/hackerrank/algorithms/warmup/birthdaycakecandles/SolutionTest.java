19bb7b79017a8158617204ab3517473ae80b1bdc
Joel Amálio <joel.amalio@gmail.com>
2018-08-23 22:02:04 -0300

    Change os tests.

--- src/test/java/br/com/joelamalio/hackerrank/algorithms/warmup/birthdaycakecandles/SolutionTest.java
+++ src/test/java/br/com/joelamalio/hackerrank/algorithms/warmup/birthdaycakecandles/SolutionTest.java
@@ -1,24 +1,19 @@
 package br.com.joelamalio.hackerrank.algorithms.warmup.birthdaycakecandles;
 
-import java.io.ByteArrayInputStream;
-
 import org.junit.Assert;
 import org.junit.Test;
 
-import br.com.joelamalio.hackerrank.algorithms.warmup.birthdaycakecandles.Solution;
 import br.com.joelamalio.hackerrank.algorithms.util.TestConsoleAb;
 
 public class SolutionTest extends TestConsoleAb {
 
 	@Test
 	public void testCase0() {
-		input("4");
-		input("3 2 1 3");
-		inContent = new ByteArrayInputStream(input.toString().getBytes());
-		System.setIn(inContent);
-		
-		Solution.main(new String[]{});
-		Assert.assertEquals(formatResult("2"), outContent.toString());
+		setInputFileByPath("warmup/birthdaycakecandles/testCase0_input.txt");
+		setOutputFileByPath("warmup/birthdaycakecandles/testCase0_output.txt");
+		Solution.main(new String[] {});
+
+		Assert.assertEquals(output.toString(), outContent.toString());
 	}
-	
+
 }
