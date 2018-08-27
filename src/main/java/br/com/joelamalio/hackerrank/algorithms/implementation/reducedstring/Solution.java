c365268b236e3b91bb5b1b780ef783807adc95da
Joel Amálio <joel.amalio@gmail.com>
2018-08-26 23:00:11 -0300

    Solution: Super Reduced String.

---
+++ src/main/java/br/com/joelamalio/hackerrank/algorithms/implementation/reducedstring/Solution.java
@@ -0,0 +1,49 @@
+package br.com.joelamalio.hackerrank.algorithms.implementation.reducedstring;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+
+public class Solution {
+
+	// Complete the superReducedString function below.
+	static String superReducedString(String s) {
+		String reduced = new String(s);
+		int i = 0;
+		int length = reduced.length();
+		
+		while (i < length - 1) {
+			if (reduced.charAt(i) == reduced.charAt(i + 1)) {
+				String target = reduced.charAt(i) + "" + reduced.charAt(i + 1);
+				reduced = reduced.replace(target, "");
+				i = 0;
+				length = reduced.length();
+			} else {
+				i++;
+			}
+		}
+		
+		if (reduced.isEmpty()) {
+			return "Empty String";
+		}
+		return reduced;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		String s = bufferedReader.readLine();
+
+		String result = superReducedString(s);
+
+		bufferedWriter.write(result);
+		bufferedWriter.newLine();
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
+}
